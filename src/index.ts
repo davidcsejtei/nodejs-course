782a8df0d5c67d37509191fcd37dd162f27e299d
csejtei1dav713 <csejtei.david@telekom.hu>
2021-05-11 09:57:16 +0200

    CORS

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,16 @@ import getAllUsers from './routes/getAllUsers';
 
 const app = express();
 app.use(express.json());
+
 const server = new http.Server(app);
 server.listen(3001);
+
+app.use((request, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+    response.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
 
 app.get('/users/all', getAllUsers);
